8f2e627423e4fa66ef0f9937ef273f3028901a49
Petri Lehtinen <petri.lehtinen@inoi.fi>
2010-02-16 09:09:23 +0200

    Add copyright notices

--- test/util.py
+++ test/util.py
@@ -1,3 +1,12 @@
+# Copyright (c) Inoi Oy 2010. All rights reserved.
+#
+# This material constitutes a trade secret of Inoi Oy.
+#
+# The software, manuals, and technical literature of
+# Inoi Oy products may not be reproduced in any form or
+# by any means except by permission in writing from
+# Inoi Oy.
+
 import os
 import sys
 import errno
