535bd44254e126a485546dbfd386e3c8f354f0ad
Petri Lehtinen <petri.lehtinen@inoi.fi>
2010-02-16 09:15:34 +0200

    Add setup.py and MANIFEST.in, update .gitignore

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='trombi',
+    version='0.1',
+    description='CouchDB client for Tornado\'s ioloop',
+    license='Proprietary',
+    author='Inoi Oy',
+    author_email='inoi@inoi.fi',
+    url='http://www.inoi.fi/',
+    packages=['trombi'],
+)
