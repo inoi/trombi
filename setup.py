908e567b39d5f2f2644e589116ec1aedf1d92934
Jyrki Pulliainen <jyrki.pulliainen@inoi.fi>
2010-08-17 13:13:44 +0300

    trombi 0.5-2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='trombi',
-    version='0.5-1',
+    version='0.5-2',
     description='CouchDB client for Tornado\'s ioloop',
     license='Proprietary',
     author='Inoi Oy',
