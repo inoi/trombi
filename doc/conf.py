5075fa2760915360094bd690bb028f5a15605885
Jyrki Pulliainen <jyrki@taiste.fi>
2011-03-31 10:49:34 +0300

    Claim copyrights from now on, as I no longer work at Inoi

--- doc/conf.py
+++ doc/conf.py
@@ -38,7 +38,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'trombi'
-copyright = u'2010, Inoi Oy'
+copyright = u'2011, Jyrki Pulliainen; 2010, Inoi Oy'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
